e6ea506935099da513c8482082d5aae6589e57a8
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-10-31 10:23:03 +0100

    i did tabs

---
+++ Tabs/script.js
@@ -0,0 +1,36 @@
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  // auto open tab
+document.getElementById("defaultOpen").click();
+
+ // to create close << X >> sign
+let closeX = document.querySelectorAll(".topright")
+//console.log(closeX);
+Array.prototype.forEach.call(closeX ,function(spanClose){
+    spanClose.onclick = function(){
+        this.parentElement.style.display= 'none'
+    }  
+})
+
+
+
+
